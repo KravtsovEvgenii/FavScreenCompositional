4c3038e338815045b0c853d8bf0e7d6a467399d3
KravtsovEvgenii <60741102+KravtsovEvgenii@users.noreply.github.com>
2021-01-31 00:15:06 +0300

    Initial Commit

---
+++ Compositional layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Compositional layout
+//
+//  Created by User on 31.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
